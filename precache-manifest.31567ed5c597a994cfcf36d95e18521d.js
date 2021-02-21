4a5db9e6101dfc43469508b3c67ef0e2e56d3e8e
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-02-21 10:55:02 -0800

    Updates

---
+++ precache-manifest.31567ed5c597a994cfcf36d95e18521d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b6cf66b2cc56526e04bbfcbc036969",
+    "url": "/YutaMoriJP/react-material-ui/index.html"
+  },
+  {
+    "revision": "02bb2f615424c76b972c",
+    "url": "/YutaMoriJP/react-material-ui/static/js/2.4cb4cca5.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/YutaMoriJP/react-material-ui/static/js/2.4cb4cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ea5a91747d473b4e7d",
+    "url": "/YutaMoriJP/react-material-ui/static/js/main.da7ea669.chunk.js"
+  },
+  {
+    "revision": "f01beeeb96302a4bf4ba",
+    "url": "/YutaMoriJP/react-material-ui/static/js/runtime-main.d40f17e6.js"
+  },
+  {
+    "revision": "f9ba305086b04af743dad54071abf31c",
+    "url": "/YutaMoriJP/react-material-ui/static/media/bayernlogo.f9ba3050.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/YutaMoriJP/react-material-ui/precache-manifest.31567ed5c597a994cfcf36d95e18521d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/YutaMoriJP/react-material-ui/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
